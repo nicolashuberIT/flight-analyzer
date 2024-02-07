9c237fe83938d5a4387a73781f54a94da1dd1b64
Nicolas Huber <info@nicolas-huber.ch>
2024-02-07 13:37:31 +0100

    dev: add static type annotations

--- src/constants.py
+++ src/constants.py
@@ -1,19 +1,19 @@
 # general
 
-VERSION = "0.1.0"
-AUTHOR = "Nicolas Huber"
-AUTHOR_EMAIL = "info@nicolas-huber.ch"
-AUTHOR_URL = "https://nicolas-huber.ch"
+VERSION: str = "0.1.0"
+AUTHOR: str = "Nicolas Huber"
+AUTHOR_EMAIL: str = "info@nicolas-huber.ch"
+AUTHOR_URL: str = "https://nicolas-huber.ch"
 
 # links
 
-GITHUB_URL = "https://github.com/nicolashuberIT/flight-analyzer"
-GITHUB_ACTIONS_URL = "https://github.com/nicolashuberIT/flight-analyzer/actions"
+GITHUB_URL: str = "https://github.com/nicolashuberIT/flight-analyzer"
+GITHUB_ACTIONS_URL: str = "https://github.com/nicolashuberIT/flight-analyzer/actions"
 
 # algorithms
 
-ANGLE_PAST_THRESHOLD = 30
-ANGLE_FUTURE_THRESHOLD = 30
-ANGLE_THRESHOLD = 20
-DIFFERENTIAL_THRESHOLD_PERCENTAGE = 0.01
-FRAC_THRESHOLD_PERCENTAGE = 0.01
+ANGLE_PAST_THRESHOLD: int = 30
+ANGLE_FUTURE_THRESHOLD: int = 30
+ANGLE_THRESHOLD: int = 20
+DIFFERENTIAL_THRESHOLD_PERCENTAGE: float = 0.01
+FRAC_THRESHOLD_PERCENTAGE: float = 0.01
