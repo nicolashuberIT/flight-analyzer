d61b83fca226145356cb824ed1f1792e62b74da8
Nicolas Huber <info@nicolas-huber.ch>
2024-02-08 09:39:25 +0100

    docs: update docstring

--- src/helpers/data_analyzer.py
+++ src/helpers/data_analyzer.py
@@ -5,5 +5,10 @@ from ..algorithms import angle_analyzer
 
 class DataAnalyzer:
     """
-    Class to loop through the data and analyze it by applying the AngleAnalyzer class.
+    Class to loop through the data and analyze it by applying the AngleAnalyzer class. The foundation of this class is a csv file containing coordinates, speed data etc. The output will be a new csv file containing the same data but flagged with an index:
+
+    - 0 = point lies on a straight line
+    - 1 = end point of a straight line
+    - 2 = end point of curve
+    - 3 = point lies on curve
     """
