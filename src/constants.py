1b2ce586ac51524039bf8e1763fca36bd554c751
Nicolas Huber <info@nicolas-huber.ch>
2024-02-20 17:23:30 +0100

    dev: run optimization

--- src/constants.py
+++ src/constants.py
@@ -20,10 +20,10 @@ ORIGINAL_REFERENCE_PATH: str = (
 # algorithms
 
 ANGLE_PAST_THRESHOLD: int = (
-    80  # number of points in the past that are considered for the angle evaluation
+    95  # number of points in the past that are considered for the angle evaluation
 )
 ANGLE_FUTURE_THRESHOLD: int = (
-    35  # number of points in the future that are considered for the angle evaluation
+    90  # number of points in the future that are considered for the angle evaluation
 )
 ANGLE_THRESHOLD: int = 20  # angle < 20° is considered as straight line
 LINEAR_REGRESSION_THRESHOLD: float = 0.9  # r-value > 0.9 is considered as straight line
@@ -42,6 +42,6 @@ R_VALUE_WEIGHT: float = 0.6  # weight of the r-value in the optimization
 P_VALUE_WEIGHT: float = 0.3  # weight of the p-value in the optimization
 STD_ERROR_WEIGHT: float = 0.1  # weight of the standard error in the optimization
 
-OPTIMIZATION_LIMIT: int = 100  # upper limit of optimization loops
+OPTIMIZATION_LIMIT: int = 200  # upper limit of optimization loops
 OPTIMIZATION_STEPS: int = 5  # step size per optimization loop
-OPTIMIZATION_RUNTIME_ESTIMATION: int = 120  # estimated runtime per loop in seconds
+OPTIMIZATION_RUNTIME_ESTIMATION: int = 6  # estimated runtime per loop in seconds
