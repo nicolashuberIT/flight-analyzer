5ba6eef223eb1a2493a9e916a1c49f9d9a4cf905
Nicolas Huber <info@nicolas-huber.ch>
2024-02-21 17:04:25 +0100

    dev (test): update testing config

--- testing.sh
+++ testing.sh
@@ -3,6 +3,7 @@
 # Automatically generated shell script to run all test files with pytest. Check update_testing.sh for further reference
 
 pytest -v "tests/test_angle_analyzer.py"
+pytest -v "tests/test_c_values_analyzer.py"
 pytest -v "tests/test_data_analyzer.py"
 pytest -v "tests/test_file_converter.py"
 pytest -v "tests/test_file_processor.py"
