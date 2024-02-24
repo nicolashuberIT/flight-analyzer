40085b06c217149b17c1091c2bd6198b7a80af44
Nicolas Huber <info@nicolas-huber.ch>
2024-02-24 17:16:36 +0100

    dev (test): quality analyzer

--- tests/test_quality_analyzer.py
+++ tests/test_quality_analyzer.py
@@ -26,6 +26,40 @@ def analyzer():
     - (QualityAnalyzer): A QualityAnalyzer object."""
     return src.helpers.quality_analyzer.QualityAnalyzer()
 
+def test_calculate_cA_approximation_original(analyzer):
+    """
+    Tests the calculate_cA_approximation_original method of the QualityAnalyzer class.
+    
+    Args:
+    - analyzer (QualityAnalyzer): The QualityAnalyzer object to test.
+    
+    Returns:
+    - None
+    """
+    assert round(27.911 * (10 ** (-1.929)), 4) == 0.3287
+    assert analyzer.calculate_cA_approximation_original(10) == 27.911 * (10 ** (-1.929))
+    assert analyzer.calculate_cA_approximation_original(20) == 27.911 * (20 ** (-1.929))
+    assert analyzer.calculate_cA_approximation_original(30) == 27.911 * (30 ** (-1.929))
+    assert analyzer.calculate_cA_approximation_original(40) == 27.911 * (40 ** (-1.929))
+    assert analyzer.calculate_cA_approximation_original(50) == 27.911 * (50 ** (-1.929))
+
+def test_calculate_cW_approximation_original(analyzer):
+    """
+    Tests the calculate_cW_approximation_original method of the QualityAnalyzer class.
+    
+    Args:
+    - analyzer (QualityAnalyzer): The QualityAnalyzer object to test.
+    
+    Returns:
+    - None
+    """
+    assert round(15.376 * (10 ** (-2.511)), 4) == 0.0474
+    assert analyzer.calculate_cW_approximation_original(10) == 15.376 * (10 ** (-2.511))
+    assert analyzer.calculate_cW_approximation_original(20) == 15.376 * (20 ** (-2.511))
+    assert analyzer.calculate_cW_approximation_original(30) == 15.376 * (30 ** (-2.511))
+    assert analyzer.calculate_cW_approximation_original(40) == 15.376 * (40 ** (-2.511))
+    assert analyzer.calculate_cW_approximation_original(50) == 15.376 * (50 ** (-2.511))
+
 def test_calculate_cA_approximation(analyzer):
     """
     Tests the calculate_cA_approximation method of the QualityAnalyzer class.
